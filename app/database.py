cae85b68ca10b891ef0a72be52f304321a73d424
Mukul Anand <kumarmukul70946@gmail.com>
2026-01-09 18:32:42 +0530

    Configure for deployment

--- app/database.py
+++ app/database.py
@@ -2,13 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+
 # Update this with your actual database URL
 # For local dev: postgresql://user:password@localhost/dbname
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/issue_tracker"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+
+connect_args = {}
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
